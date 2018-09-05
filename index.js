6aeb3c2f2babc410af07bcdd9af3b9f8e9298fe1
pooja <pooja@fenixwork.com>
2018-09-05 17:30:15 +0530

    changed timezone

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
+    currentTime.setTimezone("Asia/Kolkata");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'The time in Pune is: ' + currentTime.toString(),
     });
 };
 
